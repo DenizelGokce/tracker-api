55193fdc952ed0bcfd84973787266dbe66bd92a4
Denizel Gokce <denizel.gokce@student.graafschapcollege.nl>
2026-05-08 14:29:55 +0200

    Tests

---
+++ database/factories/TaskFactory.php
@@ -0,0 +1,16 @@
+<?php
+namespace Database\Factories;
+use Illuminate\Database\Eloquent\Factories\Factory;
+use App\Models\Task;
+
+class TaskFactory extends Factory
+{
+    protected $model = Task::class;
+    public function definition(): array
+    {
+        return [
+            'title' => fake()->sentence(3),
+            'is_done' => false,
+        ];
+    }
+}
